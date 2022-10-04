f6a0ac8225e54f4c85625f37800d1ad69486fbf1
Catherine Kleimeier <catherine.kleimeier@gmail.com>
2022-10-04 11:14:11 -0400

    fix: Edit install scripts

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,6 +1,7 @@
 import commonjs from '@rollup/plugin-commonjs'; // Convert CommonJS modules to ES6
 import vue from 'rollup-plugin-vue'; // Handle .vue SFC files
 import buble from '@rollup/plugin-buble'; // Transpile/polyfill with reasonable browser support
+import lottie from 'lottie-web';
 export default {
     input: 'src/index.js', // Path relative to package.json
     output: {
@@ -15,4 +16,5 @@ export default {
         }),
         buble(), // Transpile to ES5
     ],
+    external: ['lottie']
 };
